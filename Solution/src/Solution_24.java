57d926ff2a26b2ebc91d1604ec1d0ce51d47602d
Nannf <akmd_szc@163.com>
2020-05-30 17:25:20 +0800

    24 链表节点两两交换 递归解法

--- Solution/src/Solution_24.java
+++ Solution/src/Solution_24.java
@@ -24,17 +24,16 @@ public class Solution_24 {
         if (head == null || head.next == null) {
             return head;
         }
-        // 先记录一下首节点
-
-
         // 先找到最小的操作集，再循环操作，再定循环终止条件
-
-        // 最小操作集： a.next.next = a; a.next = a.next.next;
-        // 循环终止条件 因为交换需要至少有两个节点，所以当当前节点的next不为空时，都需要交换
-
+        // 最小操作集：
+        //  pre     head        next        next2
+        //   ↓       ↓           ↓            ↓
+        //   a       b           c            d
+        //  b.next = d; c.next = b; a.next=c; 至此 bc 交换完成，pre 指向 head，head 指向 d
+        // 循环终止条件 两两交换，当当前节点为空时或当前节点的next为空时，停止交换
         // 这个是交换之后的头节点
         ListNode newHeadNode = head.next;
-        // 每次交换之后的后一个节点需要存储起来
+        // 每次交换之后的前一个节点需要存储起来
         ListNode preNode = null;
         while (head != null) {
             // 当前节点的下一个节点
@@ -44,9 +43,10 @@ public class Solution_24 {
             }
             // 当前节点的下两个节点
             ListNode nextNextNode = nextNode.next;
-
+            // 交换后继节点
             head.next = nextNextNode;
             nextNode.next = head;
+            // 把链表连接起来
             // 第一次执行
             if (preNode == null) {
                 preNode = head;
@@ -55,11 +55,26 @@ public class Solution_24 {
                 preNode = head;
             }
             head = nextNextNode;
-
-
         }
         return newHeadNode;
     }
 
+    // 递归解法
+    public ListNode swapPairs_WithRecursion(ListNode head) {
+        // 递归终止条件
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 傻乎乎的想直接照抄链表反转的例子
+//        ListNode newNode = swapPairs_WithRecursion(head.next);
+        // 递归每次的处理应该是越来越小的
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        firstNode.next = swapPairs_WithRecursion(secondNode);
+        secondNode.next = firstNode;
+
+        return secondNode;
+    }
 
 }
