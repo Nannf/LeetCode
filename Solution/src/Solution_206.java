229ba8cdd952bd41c3221169d84b9eea19ddf259
Nannf <akmd_szc@163.com>
2020-05-31 10:54:37 +0800

    1. 新增单链表反转的两种迭代实现方式 2. 修改LRU缓存淘汰算法的相关注释

--- Solution/src/Solution_206.java
+++ Solution/src/Solution_206.java
@@ -8,6 +8,7 @@ public class Solution_206 {
     class ListNode {
         int val;
         ListNode next;
+
         ListNode(int x) {
             val = x;
         }
@@ -17,19 +18,20 @@ public class Solution_206 {
 
     /**
      * reverseList: head=1
-     *     reverseList: head=2
-     * 	    reverseList: head=3
-     * 		    reverseList:head=4
-     * 			    reverseList:head=5
-     * 					终止返回
-     * 				cur = 5
-     * 				4.next.next->4，即5->4
-     * 			cur=5
-     * 			3.next.next->3，即4->3
-     * 		cur = 5
-     * 		2.next.next->2，即3->2
-     * 	cur = 5
-     * 	1.next.next->1，即2->1
+     * reverseList: head=2
+     * reverseList: head=3
+     * reverseList:head=4
+     * reverseList:head=5
+     * 终止返回
+     * cur = 5
+     * 4.next.next->4，即5->4
+     * cur=5
+     * 3.next.next->3，即4->3
+     * cur = 5
+     * 2.next.next->2，即3->2
+     * cur = 5
+     * 1.next.next->1，即2->1
+     *
      * @param head
      * @return
      */
@@ -45,6 +47,62 @@ public class Solution_206 {
         return node1;
     }
 
+    /**
+     * 迭代解法
+     * 这个思路受链表节点交换的影响
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList_Solution2(ListNode head) {
+        // 终止条件
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 如何记录反转后的头节点
+        // 把每个节点的后继改为前驱即可
+        ListNode preNode = null;
+        // 找到终止条件,反转需要两个节点
+        while (head != null) {
+            if (head.next == null) {
+                head.next = preNode;
+                break;
+            }
+            // 反转的最小操作集 是这两个节点
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+
+            // 记录下第二个节点的后继节点,当成下一个head
+            ListNode tmpNode = secondNode.next;
+
+            // 还需要记录 firstNode 的前驱节点
+            firstNode.next = preNode;
+            secondNode.next = firstNode;
+            // 如果后继节点为空
+            if (tmpNode == null) {
+                head = secondNode;
+                break;
+            }
+            head = tmpNode;
+            preNode = secondNode;
+        }
+        return head;
+    }
+
+    public ListNode reverseList_Solution3(ListNode head) {
+        // 反转需要记录节点的前一个节点
+        ListNode preNode = null;
+        ListNode curNode = head;
+        while (curNode != null) {
+            // 防止链表中断,还需要记录下当前节点的后继节点
+            // 这就要求cruNode 不能为null
+            ListNode tmpNode = curNode.next;
+            curNode.next = preNode;
+            preNode = curNode;
+            curNode = tmpNode;
+        }
+        return preNode;
+    }
 
 
 }
--- SomeInterestingAlgorithms/src/LRUCache.java
+++ SomeInterestingAlgorithms/src/LRUCache.java
@@ -1,26 +1,27 @@
 /**
  * @author Nannf
  * @date 2020/5/30 20:00
- * @description
- * LRU 缓存淘汰算法，
+ * @description LRU 缓存淘汰算法，
  * LRU 最近最久未使用，链表版本
- * 每次加入时，判断当前链表的长度有没有到规定的长度，如果没有则插入，如果达到了，就选择最早插入的那个节点删除
  * 每次加入的时候需要判断当前插入的节点是否存在，如果存在，把那个节点删除，在用头插法插入节点
+ * 如果不存在,判断当前链表的长度有没有到规定的长度，如果没有则插到头节点中，如果达到了，就选择最早插入的那个节点删除,即尾节点
  */
 public class LRUCache {
     // 缓存的长度
     private static final int CACHE_SIZE = 3;
 
     // 准备插入的数据
-    private static final int[] READY_INSERT_ARRAY = new int[] {
-            1,2,3,1,4,5
+    private static final int[] READY_INSERT_ARRAY = new int[]{
+            1, 2, 3, 1, 4, 5
     };
 
     // 当前缓存中的数量
     private static int CURRENT_CACHE_SIZE = 0;
+
     static class ListNode {
         int x;
         ListNode next;
+
         public ListNode(int x) {
             this.x = x;
         }
@@ -31,11 +32,11 @@ public class LRUCache {
         ListNode dummyNode = new ListNode(-1);
 
         // 遍历要插入的数据
-        for (int i : READY_INSERT_ARRAY){
+        for (int i : READY_INSERT_ARRAY) {
             // 要先判断这个待插入的节点存不存在
-            if (contains(i,dummyNode)) {
+            if (contains(i, dummyNode)) {
                 // 如果在这个里面,要把这个给删了,然后插一个到头节点
-                reinsertNode(i,dummyNode);
+                reinsertNode(i, dummyNode);
                 continue;
             }
             // 如果当前缓存中不包含要加入的新节点,
@@ -44,10 +45,10 @@ public class LRUCache {
                 // 把链表的最后一个节点删除
                 deleteLastNode(dummyNode);
                 // 把这个节点插入到链表的开始
-                insertNewNode(i,dummyNode);
+                insertNewNode(i, dummyNode);
             } else {
                 // 把这个节点插入到链表的开始
-                insertNewNode(i,dummyNode);
+                insertNewNode(i, dummyNode);
                 CURRENT_CACHE_SIZE++;
             }
         }
@@ -74,9 +75,9 @@ public class LRUCache {
     private static boolean contains(int i, ListNode dummyNode) {
         ListNode cruiser = dummyNode;
         while (cruiser != null) {
-           if (cruiser.x == i) {
-               return  true;
-           }
+            if (cruiser.x == i) {
+                return true;
+            }
             cruiser = cruiser.next;
         }
         return false;
@@ -89,19 +90,19 @@ public class LRUCache {
     }
 
     private static void deleteLastNode(ListNode dummyNode) {
-            if (dummyNode.next == null) {
-                return;
-            }
-            ListNode preNode = dummyNode;
-            ListNode headNode = dummyNode.next;
-            while (headNode.next != null) {
-                preNode = headNode;
-                headNode = headNode.next;
-            }
-            preNode.next = null;
+        if (dummyNode.next == null) {
+            return;
+        }
+        ListNode preNode = dummyNode;
+        ListNode headNode = dummyNode.next;
+        while (headNode.next != null) {
+            preNode = headNode;
+            headNode = headNode.next;
+        }
+        preNode.next = null;
     }
 
-    private static void printListNode (ListNode node) {
+    private static void printListNode(ListNode node) {
         ListNode cruiser = node.next;
         while (cruiser != null) {
             System.out.println(cruiser.x);
