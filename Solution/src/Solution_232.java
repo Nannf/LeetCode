99ce848cb0b01df6e9761a808a06f82f8c06a5e1
Nannf <akmd_szc@163.com>
2020-06-02 10:03:36 +0800

    1. 修改 232  之前的实现改为MyQueue2 之前的实现使用了java自带的栈的方法，没有使用到栈和队列的特性

--- Solution/src/Solution_232.java
+++ Solution/src/Solution_232.java
@@ -11,16 +11,15 @@ import java.util.Stack;
  * empty() -- 返回队列是否为空。
  */
 public class Solution_232 {
-    class MyQueue {
+    class MyQueue2 {
         private Stack<Integer> data = new Stack<>();
-        private int size = 0;
+        private int size = 10;
         private int elementCount = 0;
 
         /**
          * Initialize your data structure here.
          */
-        public MyQueue(int size) {
-            this.size = size;
+        public MyQueue2() {
         }
 
         /**
@@ -60,6 +59,74 @@ public class Solution_232 {
                 return true;
             }
             return false;
+        }
+    }
+
+
+    class MyQueue {
+        // 两个栈,每次插入的时候,先把这个栈里的数据push到s2中,在push回来
+        // 就把后进先出变成先进先出
+        private Stack<Integer> s1 = new Stack<>();
+        private Stack<Integer> s2 = new Stack<>();
+        // 存放的队首的值
+        private int font;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyQueue() {
+        }
+
+        /**
+         * Push element x to the back of queue.
+         */
+        public void push(int x) {
+            // 如果s1 是空
+            if (s1.isEmpty()) {
+                // 队首的值,下次改变队首的值是出队的时候
+                font =x;
+                s1.push(x);
+            } else {
+                // 先把s1的值全push到s2中
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+                s1.push(x);
+                while (!s2.isEmpty()) {
+                    s1.push(s2.pop());
+                }
+            }
+
+        }
+
+        /**
+         * Removes the element from in front of queue and returns that element.
+         */
+        public int pop() {
+            if(s1.isEmpty()) {
+                return 0;
+            }
+            int value = s1.pop();
+            if (s1.isEmpty()) {
+                font = 0;
+            } else {
+                font = s1.peek();
+            }
+            return value;
+        }
+
+        /**
+         * Get the front element.
+         */
+        public int peek() {
+            return font;
+        }
+
+        /**
+         * Returns whether the queue is empty.
+         */
+        public boolean empty() {
+            return s1.isEmpty();
         }
     }
 }
