9238f874962d0045e5a2254c91ccf0d09050b1c2
Nannf <akmd_szc@163.com>
2020-06-18 23:42:33 +0800

    5 最长回文子串 优化暴力解法中不必要的遍历

--- Solution/src/Solution_5.java
+++ Solution/src/Solution_5.java
@@ -13,10 +13,52 @@ import java.util.*;
  */
 public class Solution_5 {
     public static void main(String[] args) {
-        System.out.println(new Solution_5().longestPalindrome("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"));
+        System.out.println(new Solution_5().longestPalindrome("ac"));
     }
 
     public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        // 暴力解法有出路吗？
+        // 用指针指向每个字符，然后找到和当前字符相同的下标的位置，然后判断两个相同的字符之间是不是回文串
+        // 如果是，记录下来，最后选择一个最长的进行返回
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int maxLength = 0;
+        List<String> resultList = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = chars.length - 1; j >= 0; j--) {
+                if (chars[j] == chars[i] && isPalindrome(s.substring(i, j + 1))) {
+                    if (j - i > maxLength) {
+                        maxLength = j - i;
+                        resultList.clear();
+                        resultList.add(s.substring(i, j + 1));
+                    }
+                    break;
+                }
+            }
+        }
+        if (resultList.size() > 0) {
+            return resultList.get(0);
+        } else {
+            return s.substring(0, 1);
+        }
+    }
+
+    private static boolean isPalindrome(String str) {
+        char[] c = str.toCharArray();
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (!(c[i++] == c[j--])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String longestPalindrome_NotPass(String s) {
         if (s.length() <= 1) {
             return s;
         }
@@ -60,15 +102,4 @@ public class Solution_5 {
         }
     }
 
-    private static boolean isPalindrome(String str) {
-        char[] c = str.toCharArray();
-        int i = 0;
-        int j = str.length() - 1;
-        while (i < j) {
-            if (!(c[i++] == c[j--])) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
