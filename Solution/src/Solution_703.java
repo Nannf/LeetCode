deaeed044dbaf1fcd21771c49ce4e43374e3c4cc
Nannf <akmd_szc@163.com>
2020-07-07 14:58:18 +0800

    703 topK

--- Solution/src/Solution_703.java
+++ Solution/src/Solution_703.java
@@ -17,68 +17,109 @@
  */
 public class Solution_703 {
     int k;
-    int[] nums;
-    int result;
-
-    public static void main(String[] args) {
-        int[] array = new int[]{4, 5, 8, 2};
-        Solution_703 s = new Solution_703(3, array);
-        System.out.println(s.add(3));
-        System.out.println(s.add(5));
-        System.out.println(s.add(10));
-        System.out.println(s.add(9));
-        System.out.println(s.add(4));
-    }
+    int[] data;
+    int size;
 
     public Solution_703(int k, int[] nums) {
         this.k = k;
-        this.nums = nums;
+        // 只维护k个数据的小顶堆
+        this.data = new int[k + 1];
+        init(nums);
+    }
+
+    private void init(int[] nums) {
+        if (nums.length < k) {
+            for (int i = 0; i < nums.length; i++) {
+                insert(nums[i]);
+            }
+        } else {
+            for (int i = 0; i < k; i++) {
+                insert(nums[i]);
+            }
+            for (int i = k; i < nums.length; i++) {
+                if (nums[i] > data[1]) {
+                    remove();
+                    insert(nums[i]);
+                }
+            }
+        }
+
+
+    }
+
+    public void remove() {
+        if (size == 0) {
+            System.out.println("堆已为空，无法删除");
+            return;
+        }
+        // 拿最后一个节点的值替换根节点的值
+        // 这一步的目的是为了保证树在删除后仍然是完全二叉树
+        data[1] = data[size];
+        data[size--] = 0;
+        // 然后首节点要下沉
+        heapify(data, size, 1);
+    }
+
+    public void insert(int num) {
+        // 先把数据插到数组的最后
+        data[++size] = num;
+        //小顶堆 插入进来之后 可能需要上浮，上浮的终止条件的浮到根节点
+        int head = size;
+        while (data[head] < data[head / 2] && (head / 2) > 0) {
+            // 交换节点
+            swap(data, head, head / 2);
+            head = head / 2;
+        }
+    }
+
+    private void heapify(int[] a, int size, int i) {
+        int minpos = i;
+        while (true) {
+            // 先比较左节点
+            if (i * 2 <= size && a[i * 2] < a[i]) {
+                minpos = i * 2;
+            }
+            // 再比较右节点
+            if (i * 2 + 1 <= size && a[i * 2 + 1] < a[minpos]) {
+                minpos = i * 2 + 1;
+            }
+            // 如果三个节点最大的是自己，说明下沉到了最低点
+            if (minpos == i) {
+                break;
+            }
+            //否则，交换，接着下沉
+            swap(a, i, minpos);
+            i = minpos;
+        }
+    }
+
+    public static void swap(int[] a, int x, int y) {
+        int tmp = a[x];
+        a[x] = a[y];
+        a[y] = tmp;
     }
 
 
     public int add(int val) {
-
-        return -1;
-    }
-
-
-    public int add_solution1(int val) {
-        int[] newArray = new int[nums.length + 1];
-        System.arraycopy(nums, 0, newArray, 0, nums.length);
-        newArray[nums.length] = val;
-        nums = newArray;
-        findK_solution1(nums, 0, nums.length - 1);
-        System.out.println("size + :::: " + nums.length);
-        return result;
-    }
-
-    private void findK_solution1(int[] nums, int p, int r) {
-        int q = part(nums, p, r);
-        if (q + 1 == k) {
-            result = nums[q];
-            return;
+        if (size < k) {
+            insert(val);
+            return data[1];
         }
-        if (q + 1 < k) {
-            findK_solution1(nums, q + 1, r);
-        } else {
-            findK_solution1(nums, p, q - 1);
+        if (val <= data[1]) {
+            return data[1];
         }
+        remove();
+        insert(val);
+        return data[1];
     }
 
-    private int part(int[] nums, int p, int r) {
-        int i = p;
-        int x = nums[r];
-        for (int j = p; j < r; j++) {
-            if (nums[j] > x) {
-                int tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = tmp;
-                i++;
-            }
-        }
-        int tmp = nums[i];
-        nums[i] = nums[r];
-        nums[r] = tmp;
-        return i;
+    public static void main(String[] args) {
+        Solution_703 heap = new Solution_703(1,new int[]{});
+        System.out.println(heap.add(-3));
+        System.out.println(heap.add(-2));
+        System.out.println(heap.add(-4));
+        System.out.println(heap.add(0));
+        System.out.println(heap.add(4));
     }
+
 }
