ab03b80c40b28f3ea36e0b0d5b50f35436c8eb65
Nannf <akmd_szc@163.com>
2020-07-16 16:53:38 +0800

    N皇后问题

---
+++ Solution/src/Review_0812.java
@@ -0,0 +1,104 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @auth Nannf
+ * @date 2020/7/16 15:09
+ * @description: 设计一种算法，打印 N 皇后在 N × N 棋盘上的各种摆法，其中每个皇后都不同行、不同列，也不在对角线上。这里的“对角线”指的是所有的对角线，不只是平分整个棋盘的那两条对角线。
+ * <p>
+ * 注意：本题相对原题做了扩展
+ * <p>
+ * 输入：4
+ * 输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ * 解释: 4 皇后问题存在如下两个不同的解法。
+ * [
+ *  [".Q..",  // 解法 1
+ *   "...Q",
+ *   "Q...",
+ *   "..Q."],
+ * <p>
+ *  ["..Q.",  // 解法 2
+ *   "Q...",
+ *   "...Q",
+ *   ".Q.."]
+ * ]
+ */
+public class Review_0812 {
+    List<List<String>> ans = new ArrayList<>();
+
+    public static void main(String[] args) {
+        for (List<String> str : new Review_0812().solveNQueens(2)) {
+            System.out.println(str);
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        // N皇后小于三时无解
+        if (n <= 0) {
+            return Collections.emptyList();
+        }
+        // 思路就是探索出所有的解，并对其中满足条件的解进行记录
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        down(0, n, result);
+        return ans;
+    }
+
+    // 下棋子
+    public void down(int row, int n, int[] result) {
+        // 当已经下到第n个，说明已经下完了n-1行，不需要继续下了
+        if (row == n) {
+            ans.add(build(result, n));
+            return;
+        }
+        for (int column = 0; column < n; column++) {
+            if (isOk(row, column, result, n)) {
+                result[row] = column;
+                down(row + 1, n, result);
+            }
+        }
+    }
+
+    private boolean isOk(int row, int column, int[] result, int n) {
+        int leftUp = column - 1, rightUp = column + 1;
+        while (row > 0) {
+            row--;
+            // 本列不能有值
+            if (result[row] == column) {
+                return false;
+            }
+            // 左对角线不能有值
+            if (leftUp >= 0 && result[row] == leftUp) {
+                return false;
+            }
+            // 右对角线不能有值
+            if (rightUp < n && result[row] == rightUp) {
+                return false;
+            }
+            --leftUp;
+            ++rightUp;
+        }
+        return true;
+    }
+
+    private List<String> build(int[] result, int n) {
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i : result) {
+            sb.setLength(0);
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    sb.append(".");
+                } else {
+                    sb.append("Q");
+                }
+            }
+            list.add(sb.toString());
+        }
+        return list;
+    }
+
+}
--- Solution/src/Solution_315.java
+++ Solution/src/Solution_315.java
@@ -20,16 +20,18 @@ import java.util.*;
  * [10,27,10,35,12,22,28,8,19,2,12,2,9,6,12,5,17,9,19,12,14,6,12,5,12,3,0,10,0,7,8,4,0,0,4,3,2,0,1,0]
  */
 public class Solution_315 {
+    // 索引数组
     int[] index;
+    // 结果数组
     int[] ans;
 
     public static void main(String[] args) {
-        int[] nums = new int[]{26,78,27,100,33,67,90,23,66,5,38,7,35,23,52,22,83,51,98,69,81,32,78,28,94,13,2,97,3,76,99,51,9,21,84,66,65,36,100,41};
-        System.out.println(new Solution_315().countSmaller_FailedAns(nums).toString());
+        int[] nums = new int[]{26, 78, 27, 100, 33, 67, 90, 23, 66, 5, 38, 7, 35, 23, 52, 22, 83, 51, 98, 69, 81, 32, 78, 28, 94, 13, 2, 97, 3, 76, 99, 51, 9, 21, 84, 66, 65, 36, 100, 41};
+        System.out.println(new Solution_315().countSmaller(nums).toString());
     }
 
 
-    public List<Integer> countSmaller_FailedAns(int[] nums) {
+    public List<Integer> countSmaller(int[] nums) {
         int n = nums.length;
         List<Integer> list = new ArrayList<>();
         if (n == 0) {
@@ -61,22 +63,46 @@ public class Solution_315 {
         merge(nums, l, mid, h);
     }
 
+    /**
+     * nums  [6,5,2,1]
+     * index [0,1,2,3]
+     *
+     * @param a
+     * @param l
+     * @param mid
+     * @param r
+     */
     private void merge(int[] a, int l, int mid, int r) {
         int[] tempIndex = new int[r - l + 1];
         int i = l, j = mid + 1, k = 0;
         while (i <= mid && j <= r) {
+            // 移动的是索引数组，真实的数据不变
+            // 这个跟计算总逆序度的不同，计算总逆序度时，出现一次交换，就把逆序度加一即可，本题需要计算每个数字的逆序度
+            // 因为一个数字随着对比合并的过程中是会不断移动的，每次移动 数字的逆序度都要加一，我们在移动一个数字的时候怎么定位到它的位置在原数组中的位置是多少呢？这就是这个问题的关键
+            // 之前考虑过用map来存储每个数字对应的逆序度，但是这样的实现就要求一个条件，就是数据不能重复，否则就要修改数据结构，一个数据除了保存他的值之外，还需保存他的初始索引
+            // 还有一种思路就是用一个索引数组，每次只移动索引数组，实际上原数组的数据是不变的
+            // a      [5,2,6,1]
+            // index  [0,1,2,3]
+            // 第一次归并之后:
+            // index  [1,0,2,3]
+            // index  [1,0,3,2]
+            // index  [3,1,0,2]
+            //  如果左边比右边小，把左边的指针移动
             if (a[index[i]] <= a[index[j]]) {
                 tempIndex[k++] = index[i++];
             } else {
+                // 否则，移动右边的指针
                 tempIndex[k++] = index[j++];
+                // 左边的指针i的逆序度+1
                 ans[index[i]] += 1;
+                // 左边到mid的所有数字的逆序度都要+1
                 int tmp = i + 1;
                 while (tmp <= mid) {
                     ans[index[tmp++]] += 1;
                 }
             }
         }
-        for (; i <= mid;) {
+        for (; i <= mid; ) {
             tempIndex[k++] = index[i++];
         }
         for (; j <= r; ) {
