9a6961f697170e0f9bf30214b065c1abf677ffd4
Nannf <akmd_szc@163.com>
2020-07-17 18:10:29 +0800

    0-1 背包 带价格 未完成

--- SomeInterestingAlgorithms/src/PackageProblem.java
+++ SomeInterestingAlgorithms/src/PackageProblem.java
@@ -9,6 +9,7 @@ public class PackageProblem {
     // 回溯算法实现。注意：我把输入的变量都定义成了成员变量。
     private int maxW = Integer.MIN_VALUE; // 结果放到maxW中
     private int[] weight = {2, 2, 4, 6, 2};  // 物品重量
+    private int[] value = {3, 4, 8, 9, 6}; // 物品的价值
     private int n = 5; // 物品个数
     private int w = 9; // 背包承受的最大重量
 
@@ -47,8 +48,34 @@ public class PackageProblem {
                 }
             }
         }
-        for (int i = w; i>=0;i--) {
-            if (dp[n-1][i] == 1) {
+        for (int i = w; i >= 0; i--) {
+            if (dp[n - 1][i] == 1) {
+                maxW = i;
+                break;
+            }
+        }
+    }
+
+    public void dpf_withValue() {
+        int ans = Integer.MIN_VALUE;
+        int[][] dp = new int[n][w + 1];
+        // 对第一行进行初始化
+        dp[0][0] = 1;
+        if (weight[0] <= w) {
+            dp[0][weight[0]] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < w + 1; j++) {
+                if (dp[i - 1][j] != 0) {
+                    dp[i][0 + j] = 1;
+                    if (weight[i] + j <= w) {
+                        dp[i][weight[i] + j] = 1;
+                    }
+                }
+            }
+        }
+        for (int i = w; i >= 0; i--) {
+            if (dp[n - 1][i] == 1) {
                 maxW = i;
                 break;
             }
