542eb9cd8c12b58842667c22b932f5b0db1d6ad6
Nannf <akmd_szc@163.com>
2020-07-17 17:51:30 +0800

    0-1 背包 动态规划 动态规划天下第一 回溯沙雕

--- SomeInterestingAlgorithms/src/PackageProblem.java
+++ SomeInterestingAlgorithms/src/PackageProblem.java
@@ -5,26 +5,53 @@
  */
 public class PackageProblem {
 
-    public static int maxW = Integer.MIN_VALUE; //存储背包中物品总重量的最大值
+
+    // 回溯算法实现。注意：我把输入的变量都定义成了成员变量。
+    private int maxW = Integer.MIN_VALUE; // 结果放到maxW中
+    private int[] weight = {2, 2, 4, 6, 2};  // 物品重量
+    private int n = 5; // 物品个数
+    private int w = 9; // 背包承受的最大重量
 
     public static void main(String[] args) {
-        int[] items = new int[]{98, 45, 44, 3, 1};
-        f(0, 0, items, 5, 100);
-        System.out.println(maxW);
+        PackageProblem pp = new PackageProblem();
+        pp.dpf();
+        System.out.println(pp.maxW);
     }
 
-    // cw表示当前已经装进去的物品的重量和；i表示考察到哪个物品了；
-// w背包重量；items表示每个物品的重量；n表示物品个数
-// 假设背包可承受重量100，物品个数10，物品重量存储在数组a中，那可以这样调用函数：
-// f(0, 0, a, 10, 100)
-    public static void f(int i, int cw, int[] items, int n, int w) {
-        if (cw == w || i == n) { // cw==w表示装满了;i==n表示已经考察完所有的物品
+    public void f(int i, int cw) { // 调用f(0, 0)
+        if (cw == w || i == n) { // cw==w表示装满了，i==n表示物品都考察完了
             if (cw > maxW) maxW = cw;
             return;
         }
-        f(i + 1, cw, items, n, w);
-        if (cw + items[i] <= w) {// 已经超过可以背包承受的重量的时候，就不要再装了
-            f(i + 1, cw + items[i], items, n, w);
+        f(i + 1, cw); // 选择不装第i个物品
+        if (cw + weight[i] <= w) {
+            f(i + 1, cw + weight[i]); // 选择装第i个物品
+        }
+    }
+
+    public void dpf() {
+        int ans = Integer.MIN_VALUE;
+        int[][] dp = new int[n][w + 1];
+        // 对第一行进行初始化
+        dp[0][0] = 1;
+        if (weight[0] <= w) {
+            dp[0][weight[0]] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < w + 1; j++) {
+                if (dp[i - 1][j] != 0) {
+                    dp[i][0 + j] = 1;
+                    if (weight[i] + j <= w) {
+                        dp[i][weight[i] + j] = 1;
+                    }
+                }
+            }
+        }
+        for (int i = w; i>=0;i--) {
+            if (dp[n-1][i] == 1) {
+                maxW = i;
+                break;
+            }
         }
     }
 }
