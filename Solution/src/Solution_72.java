70c09b7fbff2be5b3103429f398a1c92c49211c8
Nannf <akmd_szc@163.com>
2020-07-21 14:43:11 +0800

    72 编辑距离 动态规划

---
+++ Solution/src/Solution_72.java
@@ -0,0 +1,51 @@
+/**
+ * @auth Nannf
+ * @date 2020/7/21 14:24
+ * @description: 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * <p>
+ * 你可以对一个单词进行如下三种操作：
+ * <p>
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ * <p>
+ * <p>
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/edit-distance
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution_72 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution_72().minDistance("horse", "ros"));
+    }
+
+    public int minDistance(String word1, String word2) {
+        int l1 = word1.length();
+        int l2 = word2.length();
+        int[][] dp = new int[l1 + 1][l2 + 1];
+        for (int i = 0; i <= l1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= l2; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i][j - 1]), dp[i - 1][j]) + 1;
+                }
+            }
+        }
+        return dp[l1][l2];
+    }
+}
---
+++ Solution/src/Solution_95.java
@@ -0,0 +1,55 @@
+import java.util.List;
+
+/**
+ * @auth Nannf
+ * @date 2020/7/21 9:21
+ * @description: 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ * <p>
+ * <p>
+ * 输入：3
+ * 输出：
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * 解释：
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution_95 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        return null;
+    }
+}
