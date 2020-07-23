6c7e219c7bf6d2a379c910e9f2300437a4eb72e6
Nannf <akmd_szc@163.com>
2020-07-23 11:18:56 +0800

    回溯 784 增加一些对回溯算法的理解

--- Solution/src/Solution_784.java
+++ Solution/src/Solution_784.java
@@ -30,6 +30,12 @@ public class Solution_784 {
         }
     }
 
+    /**
+     *
+     * @param S
+     * @return
+     * 回溯是有套路的
+     */
     public List<String> letterCasePermutation(String S) {
         List<String> result = new ArrayList<>();
         char[] choices = S.toCharArray();
@@ -51,11 +57,16 @@ public class Solution_784 {
         }
 
         // 遍历选择
+        // 这边通过index 来隐形的控制了要跳过哪些字段
+        // 第一个版本在写的时候 通过track的长度和i的大小的关系来判断的，但是实际上不对，因为track在程序运行的过程中是一直变化的，i也是一直变化的，如果靠人脑去把这种两个都是变化的情况考虑清楚
+        // 是不太现实的，如果对也是懵对的 所以这边在对所有的情况进行遍历时 要跳过一些已经遍历过的情况 就需要细细的考量，像本例中，每次在递归调用的时候，如果用i表示指针的话，每调用一次递归
+        // i的指针都要往后移动一位，这是肯定的。而且不容易出错，也是理所当然的，所以这边使用了i的值来做判断
         for (int i = index; i < choices.length; i++) {
             if (Character.isDigit(choices[i])) {
                 track.append(choices[i]);
                 backtrack(track, choices, i + 1, result);
                 // 删除刚刚的选择
+                // 这边的回退也是简单的
                 track.setLength(track.length() - 1);
             } else {
                 track.append(Character.toLowerCase(choices[i]));
