2642314e6691e804b817dbdba719e096090b1638
Nannf <akmd_szc@163.com>
2020-08-07 14:05:35 +0800

    100 递归

---
+++ Solution/src/Solution_100.java
@@ -0,0 +1,70 @@
+/**
+ * @auth Nannf
+ * @date 2020/8/7 13:46
+ * @description:
+ *
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 输入:       1         1
+ *           / \       / \
+ *          2   3     2   3
+ *
+ *         [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ *
+ * 输入:      1          1
+ *           /           \
+ *          2             2
+ *
+ *         [1,2],     [1,null,2]
+ *
+ * 输出: false
+ *
+ * 输入:       1         1
+ *           / \       / \
+ *          2   1     1   2
+ *
+ *         [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ *
+ */
+public class Solution_100 {
+
+     class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null && q != null) {
+            return false;
+        }
+        if (p != null && q== null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        boolean leftResult = isSameTree(p.left,q.left);
+        if (!leftResult) {
+            return false;
+        }
+        boolean rightResult = isSameTree(p.right,q.right);
+        return rightResult;
+    }
+}
