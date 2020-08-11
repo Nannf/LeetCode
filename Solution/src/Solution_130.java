c96f90480c51aeb13fb674a557403f0abc66688b
Nannf <akmd_szc@163.com>
2020-08-11 19:57:16 +0800

    130 对比官方和我的解法

--- Solution/src/Solution_130.java
+++ Solution/src/Solution_130.java
@@ -36,6 +36,48 @@ public class Solution_130 {
         System.out.println(1);
     }
 
+    int n, m;
+
+    public void solve_Public(char[][] board) {
+        n = board.length;
+        if (n == 0) {
+            return;
+        }
+        m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, m - 1);
+        }
+        for (int i = 1; i < m - 1; i++) {
+            dfs(board, 0, i);
+            dfs(board, n - 1, i);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    // 官方的解中，把这个函数 定义的和我差不多，只不过它的入参都是边界上的值
+    // 后面以这些边界上的值为起点，寻找该点上下左右为O的点，然后同化
+    // 同样的代码我花了官方解法两倍的代码量，主要是我对边框的点做了特殊处理，
+    public void dfs(char[][] board, int x, int y) {
+        if (x < 0 || x >= n || y < 0 || y >= m || board[x][y] != 'O') {
+            return;
+        }
+        board[x][y] = 'A';
+        dfs(board, x + 1, y);
+        dfs(board, x - 1, y);
+        dfs(board, x, y + 1);
+        dfs(board, x, y - 1);
+    }
+
+
 
     public void solve(char[][] board) {
         int n = board.length;
