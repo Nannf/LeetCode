b69527e1ea791a654807768f009378ffa398f04e
Nannf <akmd_szc@163.com>
2020-09-21 22:47:48 +0800

    47 剪枝

--- Solution/src/Solution_47.java
+++ Solution/src/Solution_47.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @auth Nannf
@@ -19,16 +16,19 @@ import java.util.List;
 public class Solution_47 {
 
     public static void main(String[] args) {
-        int[] nums = {1,1,2};
+        int[] nums = {1, 1, 2};
         new Solution_47().permuteUnique(nums);
     }
 
+    // 重复的本质是因为给定的列表中有重复的元素，导致在做选择时，选择下标A的结果，和选择下标B的结果一模一样
+    // 造成了子树的重复问题，因为全排列可以理解为，不同的根到叶子节点的路径集合
+    // 我们在判断下一个数需不需要取的时候，我们需要
     public List<List<Integer>> permuteUnique(int[] nums) {
         if (nums.length == 0) {
             return new ArrayList<>();
         }
         Arrays.sort(nums);
-        // 这边保存的都是下标索引
+
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> trace = new ArrayList<>();
         backtrace(ans, trace, nums);
@@ -37,49 +37,25 @@ public class Solution_47 {
 
     }
 
-    private List<List<Integer>> convert(List<List<Integer>> ans, int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        for (List<Integer> list : ans) {
-            List<Integer> tmp = new ArrayList<>();
-            for (int i : list) {
-                tmp.add(nums[i]);
-            }
-            result.add(tmp);
-        }
-        return result;
-    }
-
     private void backtrace(List<List<Integer>> ans, List<Integer> trace, int[] nums) {
         if (trace.size() == nums.length) {
-            if (unique(trace, ans,nums)) {
-               List<Integer> list = new ArrayList<>();
-               for (int i : trace) {
-                   list.add(nums[i]);
-               }
-               ans.add(list);
+            List<Integer> list = new ArrayList<>();
+            for (int i : trace) {
+                list.add(nums[i]);
             }
+            ans.add(list);
         }
         for (int i = 0; i < nums.length; i++) {
             if (trace.contains(i)) {
                 continue;
             }
-            trace.add(i);
-            backtrace(ans,trace,nums);
-            trace.remove(trace.size() -1);
-        }
-    }
-
-    private boolean unique(List<Integer> trace, List<List<Integer>> ans, int[] nums) {
-        Loop1:
-        for (List<Integer> list : ans) {
-            for (int i = 0; i < list.size(); i++) {
-                if (nums[trace.get(i).intValue()] != list.get(i)) {
-                    continue Loop1;
-                }
+            if (i > 0 && nums[i] == nums[i - 1] && !trace.contains(i - 1)) {
+                continue;
             }
-            return false;
+            trace.add(i);
+            backtrace(ans, trace, nums);
+            trace.remove(trace.size() - 1);
         }
-        return true;
     }
 
 }
