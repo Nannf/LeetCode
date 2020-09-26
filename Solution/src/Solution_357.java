275e9dc50c055cca350d1c7a1fe5e7a127b7967c
Nannf <akmd_szc@163.com>
2020-09-26 17:50:48 +0800

    357 动态规划，

--- Solution/src/Solution_357.java
+++ Solution/src/Solution_357.java
@@ -13,8 +13,36 @@ public class Solution_357 {
         System.out.println(new Solution_357().countNumbersWithUniqueDigits(2));
     }
 
-    // 暴力解法，超时
+
     public int countNumbersWithUniqueDigits(int n) {
+        if(n ==0) {
+            return 1;
+        }
+
+        int maxLen = Math.min(n, 10);
+        // 感觉这个不能从数字的大小着手，因为太大了
+        // 可以从长度着手
+        // 如果是一位数，那么要除去的个数是0；
+        // 如果是两位数，需要出去的有9个，之所以有九个是因为
+        // 如果是三位数，需要除去的有多少呢？
+        int[] dp = new int[maxLen + 1];
+        dp[0] = 1;
+        dp[1] = 9;
+        int sum = dp[0] + dp[1];
+        for (int i = 2; i <= maxLen; i++) {
+            // dp[i] 表示之前已经用了i-1个数，还剩10-i +1 个数
+            // 假设dp[i-1] 一共有k个数，那么对每个数而言，其前面都有11-i个选择，但是要去除那些答案中开始是包含0的解
+            // 但是我们怎么找到之前没有放0的记录有多少条呢
+
+            dp[i] = dp[i-1] * (11 - i) ;
+            sum += dp[i];
+        }
+        return sum;
+    }
+
+
+    // 暴力解法，超时，
+    public int countNumbersWithUniqueDigits_bruce(int n) {
         for (double i = 0d; i < Math.pow(10, n); i++) {
             if (occured(i)) {
                 continue;
