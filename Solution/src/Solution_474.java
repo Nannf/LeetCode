ac1a5529f38172c18e268acc60297cde4cb2dcc0
Nannf <akmd_szc@163.com>
2020-10-01 19:51:34 +0800

    474 换一种方式的回溯 但是还是超时

--- Solution/src/Solution_474.java
+++ Solution/src/Solution_474.java
@@ -25,22 +25,34 @@ import java.util.List;
  * ["10","0001","111001","1","0"]
  * 3
  * 4
- *
+ * <p>
  * ["10","0001","111001","1","0"]
  * 4
  * 3
- *
+ * <p>
  * ["11111","100","1101","1101","11000"]
  * 5
  * 7
+ * <p>
+ * ["0","1101","01","00111","1","10010","0","0","00","1","11","0011"]
+ * 63
+ * 36
+ *
+ * ["0","11","1000","01","0","101","1","1","1","0","0","0","0","1","0","0110101","0","11",
+ * "01","00","01111","0011","1","1000","0","11101","1","0","10","0111"]
+ * 9
+ * 80
  */
 public class Solution_474 {
 
     public static void main(String[] args) {
-        String[] strs = {"11111","100","1101","1101","11000"};
-        System.out.println(new Solution_474().findMaxForm(strs, 5, 7));
+        String[] strs = {"0","11","1000","01","0","101","1","1","1","0","0","0","0","1","0","0110101","0","11","01","00","01111","0011","1","1000","0","11101","1","0","10","0111"};
+        System.out.println(new Solution_474().findMaxForm(strs, 9, 80));
+        System.out.println("totalCount : " + totalCount + " , skipCount : " + skipCount);
     }
 
+    static int totalCount = 0;
+    static int skipCount = 0;
     int ans = 0;
 
     // 动态规划，由小推大
@@ -52,27 +64,33 @@ public class Solution_474 {
     // 我们可以单纯的使用m和n作为备忘录的条件吗
     // 其实是不可以的，我们需要把已做的选择
     public int findMaxForm(String[] strs, int m, int n) {
+        List<List<Integer>> memo = new ArrayList<>();
         // 这是回溯过程中的轨迹
         List<Integer> trace = new ArrayList<>();
-        backtrace(trace, strs, m, n);
+        backtrace(trace, strs, m, n, memo, 0);
         return ans;
     }
 
-    private void backtrace(List<Integer> trace, String[] strs, int m, int n) {
+    private void backtrace(List<Integer> trace, String[] strs, int m, int n, List<List<Integer>> memo, int s) {
+        // 我们在一开始需要判断，当前的情况我们有没有处理过
+        if (contains(new ArrayList<>(trace), memo)) {
+            return;
+        }
+        memo.add(new ArrayList<>(trace));
         // 结束条件是给定的0和1 全部用完了 或者 已经遍历到数组的最后了
-        if ((m <= 0 && n <= 0) || judge(trace,strs.length -1)) {
+        if ((m <= 0 && n <= 0) || judge(trace, strs.length - 1) || trace.size() == strs.length || s == strs.length) {
             ans = Math.max(ans, trace.size());
             return;
         }
-        for (int i = 0; i < strs.length; i++) {
-            if (trace.contains(i)) {
+        for (; s < strs.length; s++) {
+            if (trace.contains(s)) {
                 continue;
             }
-            int oneNum = countOneNumber(strs[i].toCharArray(), 1);
-            int zeroNum = countOneNumber(strs[i].toCharArray(), 0);
-            if (m >= zeroNum && n >= oneNum ) {
-                trace.add(i);
-                backtrace(trace, strs, m - zeroNum, n - oneNum);
+            int oneNum = countOneNumber(strs[s].toCharArray(), 1);
+            int zeroNum = countOneNumber(strs[s].toCharArray(), 0);
+            if (m >= zeroNum && n >= oneNum) {
+                trace.add(s);
+                backtrace(trace, strs, m - zeroNum, n - oneNum, memo, s + 1);
                 trace.remove(trace.size() - 1);
             } else {
                 ans = Math.max(ans, trace.size());
@@ -80,10 +98,26 @@ public class Solution_474 {
         }
     }
 
-    private boolean judge(List<Integer> trace,int len) {
+    private boolean contains(List<Integer> trace, List<List<Integer>> memo) {
+        for (List<Integer> list : memo) {
+            if (list.size() == trace.size()) {
+                Collections.sort(trace);
+                Collections.sort(list);
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i) != trace.get(i)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean judge(List<Integer> trace, int len) {
         int max = 0;
         for (int i : trace) {
-            max = Math.max(max,i);
+            max = Math.max(max, i);
         }
         return max == len;
     }
