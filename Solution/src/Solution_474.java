087bbdfc9fdf8062efcb05ae8970594a9db9795d
Nannf <akmd_szc@163.com>
2020-10-02 18:17:21 +0800

    474 记忆化递归，但是测试用例不通过 我还不知道原因

--- Solution/src/Solution_474.java
+++ Solution/src/Solution_474.java
@@ -37,89 +37,63 @@ import java.util.List;
  * ["0","1101","01","00111","1","10010","0","0","00","1","11","0011"]
  * 63
  * 36
- *
+ * <p>
  * ["0","11","1000","01","0","101","1","1","1","0","0","0","0","1","0","0110101","0","11",
  * "01","00","01111","0011","1","1000","0","11101","1","0","10","0111"]
  * 9
  * 80
+ * <p>
+ * ["011","1","11","0","010","1","10","1","1","0","0","0","01111","011","11","00",
+ * "11","10","1","0","0","0","0","101","001110","1","0","1","0","0","10","00100",
+ * "0","10","1","1","1","011","11","11","10","10","0000","01","1","10","0"]
+ * 44
+ * 39
  */
 public class Solution_474 {
 
     public static void main(String[] args) {
-        String[] strs = {"0","11","1000","01","0","101","1","1","1","0","0","0","0","1","0","0110101","0","11","01","00","01111","0011","1","1000","0","11101","1","0","10","0111"};
-        System.out.println(new Solution_474().findMaxForm(strs, 9, 80));
-        System.out.println("totalCount : " + totalCount + " , skipCount : " + skipCount);
+        String[] strs = {"011", "1", "11", "0", "010", "1", "10", "1", "1", "0", "0", "0", "01111", "011", "11", "00", "11", "10", "1", "0", "0", "0", "0", "101", "001110", "1", "0", "1", "0", "0", "10", "00100", "0", "10", "1", "1", "1", "011", "11", "11", "10", "10", "0000", "01", "1", "10", "0"};
+        System.out.println(new Solution_474().findMaxForm(strs, 44, 39));
     }
 
-    static int totalCount = 0;
-    static int skipCount = 0;
     int ans = 0;
 
-    // 动态规划，由小推大
-    // 第一步就是建议每一步的状态
-    // 本题回溯可以解
-    // 先尝试使用回溯进行解法,不出意外，回溯超时
-    // 回溯超时，就需要用备忘录了
-    // 备忘录是为了不重复计算，哪些属于重复计算呢？
-    // 我们可以单纯的使用m和n作为备忘录的条件吗
-    // 其实是不可以的，我们需要把已做的选择
     public int findMaxForm(String[] strs, int m, int n) {
-        List<List<Integer>> memo = new ArrayList<>();
-        // 这是回溯过程中的轨迹
+        int[][][] memo = new int[strs.length + 1][m + 1][n + 1];
         List<Integer> trace = new ArrayList<>();
-        backtrace(trace, strs, m, n, memo, 0);
+        backtrace(trace, strs, m, n, 0, memo);
         return ans;
     }
 
-    private void backtrace(List<Integer> trace, String[] strs, int m, int n, List<List<Integer>> memo, int s) {
-        // 我们在一开始需要判断，当前的情况我们有没有处理过
-        if (contains(new ArrayList<>(trace), memo)) {
+    private void backtrace(List<Integer> trace, String[] strs, int m, int n, int s, int[][][] memo) {
+        if (trace.size() >= 43) {
+            System.out.println("回溯的轨迹是：" + trace);
+        }
+        if (memo[s][m][n] != 0) {
             return;
         }
-        memo.add(new ArrayList<>(trace));
-        // 结束条件是给定的0和1 全部用完了 或者 已经遍历到数组的最后了
-        if ((m <= 0 && n <= 0) || judge(trace, strs.length - 1) || trace.size() == strs.length || s == strs.length) {
+        memo[s][m][n] = 1;
+        // 如果0和1的数量全部用完了，或者没用完，但是已经
+        if ((m <= 0 && n <= 0) || s == strs.length) {
             ans = Math.max(ans, trace.size());
             return;
         }
         for (; s < strs.length; s++) {
-            if (trace.contains(s)) {
-                continue;
-            }
             int oneNum = countOneNumber(strs[s].toCharArray(), 1);
             int zeroNum = countOneNumber(strs[s].toCharArray(), 0);
+            // 只有m和n同时满足需求，才可以加到最终的结果集中
             if (m >= zeroNum && n >= oneNum) {
                 trace.add(s);
-                backtrace(trace, strs, m - zeroNum, n - oneNum, memo, s + 1);
+                backtrace(trace, strs, m - zeroNum, n - oneNum, s + 1, memo);
+                if (trace.size() == 13) {
+                    System.out.println("bingo!");
+                }
                 trace.remove(trace.size() - 1);
             } else {
+                // 如果有一个不满足，就不能继续回溯
                 ans = Math.max(ans, trace.size());
             }
         }
-    }
-
-    private boolean contains(List<Integer> trace, List<List<Integer>> memo) {
-        for (List<Integer> list : memo) {
-            if (list.size() == trace.size()) {
-                Collections.sort(trace);
-                Collections.sort(list);
-                for (int i = 0; i < list.size(); i++) {
-                    if (list.get(i) != trace.get(i)) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean judge(List<Integer> trace, int len) {
-        int max = 0;
-        for (int i : trace) {
-            max = Math.max(max, i);
-        }
-        return max == len;
     }
 
     private int countOneNumber(char[] chars, int num) {
