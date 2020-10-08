c60edb6f9e80d5274f15acfe127f259087e51950
Nannf <akmd_szc@163.com>
2020-10-08 15:18:36 +0800

    746 初步动态规划

---
+++ Solution/src/Solution_746.java
@@ -0,0 +1,48 @@
+/**
+ * @auth Nannf
+ * @date 2020/10/8 14:51
+ * @description: 数组的每个索引作为一个阶梯，第 i个阶梯对应着一个非负数的体力花费值 cost[i](索引从0开始)。
+ * <p>
+ * 每当你爬上一个阶梯你都要花费对应的体力花费值，然后你可以选择继续爬一个阶梯或者爬两个阶梯。
+ * <p>
+ * 您需要找到达到楼层顶部的最低花费。在开始时，你可以选择从索引为 0 或 1 的元素作为初始阶梯。
+ * <p>
+ * <p>
+ * 输入: cost = [10, 15, 20]
+ * 输出: 15
+ * 解释: 最低花费是从cost[1]开始，然后走两步即可到阶梯顶，一共花费15。
+ * <p>
+ * 输入: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * 输出: 6
+ * 解释: 最低花费方式是从cost[0]开始，逐个经过那些1，跳过cost[3]，一共花费6。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution_746 {
+
+    public static void main(String[] args) {
+        int[] cost = {10, 15, 20};
+        System.out.println(new Solution_746().minCostClimbingStairs(cost));
+    }
+
+
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost.length <= 1) {
+            return 0;
+        }
+        if (cost.length == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+}
