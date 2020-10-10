c388fdb418d99d27be88e7dab00db7fde31d1a0f
Nannf <akmd_szc@163.com>
2020-10-10 22:46:53 +0800

    276这更像是排列组合

--- Solution/src/Solution_276.java
+++ Solution/src/Solution_276.java
@@ -26,34 +26,48 @@ import java.util.List;
  */
 public class Solution_276 {
     public static void main(String[] args) {
-        System.out.println(new Solution_276().numWays(3,3));
+        System.out.println(new Solution_276().numWays(4, 3));
     }
-
 
 
     public int numWays(int n, int k) {
-        if (n == 0 || k ==0) {
+        if (n == 0 || k == 0) {
             return 0;
         }
         if (n <= 2) {
-            return (int) Math.pow(k,n);
+            return (int) Math.pow(k, n);
         }
-        int[][] dp = new int[n+1][k+1];
-        dp[1][k] = k;
-        dp[2][k] = k*k;
-        for (int i = 3; i<=n;i++) {
-            // 每次在新增一个柱子的时候，新增柱子上的颜色都可以是k中的任何一个，但是要去除上一个情况下能组成两个连续的情况
-            dp[i][k] = Math.max(k*dp[i-1][k] - (i-1),0);
+        int[][] dp = new int[n + 1][k + 1];
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[i][j] = (int) Math.pow(j, i);
+            }
+        }
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                // 每次在新增一个柱子的时候，新增柱子上的颜色都可以是k中的任何一个，但是要去除上一个情况下能组成两个连续的情况
+                // 这个两个连续的数量有多少呢？
+                // 拿 n=4,k=3 来说，当我们再涂第四个柱子的时候，因为再涂第四个的时候有3个选择
+                // 如果不考虑不合法的情况 应该是 k (3) * dp[3][3]的数量，但是以 11 22 33 打头的每个都会出现一个非法数据
+                // 那这个非法数据的数量是多少呢？当数量是三的时候，这个数量是6 112 113 221 223 331 332 这样
+                // 当我们再做第五根柱子的时候，需要去判断前四根柱子中分别以 11 22 33打头的 1121 1122 1123  1131 1132 1133  （少了3个 1111 1112 1113）
+                // 同样的对2211 2212 2213 2231 2232 2233
+                // 3311 3312 3313 3321 3322 3323
+                // 如果是6 跟柱子 11211 11212 11213 11221 11223 11
+                // 3(颜色的数量) * 3(颜色的数量)  *
+                dp[i][j] = Math.max(j * dp[i - 1][j] - ((j) * (j) * (j-1)), 0);
+            }
         }
         return dp[n][k];
     }
+
 
     // 回溯可解，但是时间复杂都是k^n 贼高，先试试回溯会不会超时
     // 回溯最后还需要去重
     // 先试试把
     // 超出了内存限制
     public int numWays_backtrace(int n, int k) {
-        if (n == 0 || k ==0) {
+        if (n == 0 || k == 0) {
             return 0;
         }
         List<List<Integer>> ans = new ArrayList<>();
@@ -65,7 +79,7 @@ public class Solution_276 {
     private void backtrace(List<List<Integer>> ans, List<Integer> trace, int n, int k, int i) {
         if (i == n) {
             List<Integer> tmp = new ArrayList<>(trace);
-            if (!judgeUniqe(ans,tmp)) {
+            if (!judgeUniqe(ans, tmp)) {
                 ans.add(tmp);
             }
 
