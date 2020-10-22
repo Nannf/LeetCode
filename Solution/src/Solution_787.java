106c28c367111c831d1240680bc0d3b78b0bae37
Nannf <akmd_szc@163.com>
2020-10-22 20:43:56 +0800

    787 递归转动态规划 未完成版

--- Solution/src/Solution_787.java
+++ Solution/src/Solution_787.java
@@ -27,12 +27,11 @@ import java.util.*;
  */
 public class Solution_787 {
     public static void main(String[] args) {
-        int[][] flights = {{0,1,1},{0,2,5},{1,2,1},{2,3,1}};
-        System.out.println(new Solution_787().findCheapestPrice(4, flights, 0, 3, 1));
+        int[][] flights = {{0,12,28},{5,6,39},{8,6,59},{13,15,7},{13,12,38},{10,12,35},{15,3,23},{7,11,26},{9,4,65},{10,2,38},{4,7,7},{14,15,31},{2,12,44},{8,10,34},{13,6,29},{5,14,89},{11,16,13},{7,3,46},{10,15,19},{12,4,58},{13,16,11},{16,4,76},{2,0,12},{15,0,22},{16,12,13},{7,1,29},{7,14,100},{16,1,14},{9,6,74},{11,1,73},{2,11,60},{10,11,85},{2,5,49},{3,4,17},{4,9,77},{16,3,47},{15,6,78},{14,1,90},{10,5,95},{1,11,30},{11,0,37},{10,4,86},{0,8,57},{6,14,68},{16,8,3},{13,0,65},{2,13,6},{5,13,5},{8,11,31},{6,10,20},{6,2,33},{9,1,3},{14,9,58},{12,3,19},{11,2,74},{12,14,48},{16,11,100},{3,12,38},{12,13,77},{10,9,99},{15,13,98},{15,12,71},{1,4,28},{7,0,83},{3,5,100},{8,9,14},{15,11,57},{3,6,65},{1,3,45},{14,7,74},{2,10,39},{4,8,73},{13,5,77},{10,0,43},{12,9,92},{8,2,26},{1,7,7},{9,12,10},{13,11,64},{8,13,80},{6,12,74},{9,7,35},{0,15,48},{3,7,87},{16,9,42},{5,16,64},{4,5,65},{15,14,70},{12,0,13},{16,14,52},{3,10,80},{14,11,85},{15,2,77},{4,11,19},{2,7,49},{10,7,78},{14,6,84},{13,7,50},{11,6,75},{5,10,46},{13,8,43},{9,10,49},{7,12,64},{0,10,76},{5,9,77},{8,3,28},{11,9,28},{12,16,87},{12,6,24},{9,15,94},{5,7,77},{4,10,18},{7,2,11},{9,5,41}};
+        System.out.println(new Solution_787().findCheapestPrice(17, flights, 13, 4, 13));
     }
 
-
-
+    int minTotal = Integer.MAX_VALUE;
 
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
         Graph graph = new Graph(n);
@@ -45,10 +44,18 @@ public class Solution_787 {
         boolean[][] visited = new boolean[n][n];
 
         // dp[i][j] 表示从i->j 在转机次数不超过k次的最小距离
-        int[][] dp = new int[n][n];
+        int[][][] dp = new int[n][n][K + 2];
 
-        Arrays.fill(dp[src],Integer.MAX_VALUE);
-        dp[src][src] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= K + 1; j++) {
+                dp[src][i][j] = Integer.MAX_VALUE;
+            }
+
+        }
+        for (int i = 0; i <= K + 1; i++) {
+            dp[src][src][i] = 0;
+        }
+
 
         // 广度优先遍历
         Queue<Integer> queue = new ArrayDeque<>();
@@ -60,13 +67,21 @@ public class Solution_787 {
             while (!queue.isEmpty()) {
                 int tmp = queue.poll();
                 LinkedList<LineInfo> lineInfos = graph.adj[tmp];
+                Loop1:
                 for (LineInfo lineInfo : lineInfos) {
                     if (visited[tmp][lineInfo.v]) {
                         continue;
                     }
                     visited[tmp][lineInfo.v] = true;
-                    dp[src][lineInfo.v] = Math.min(dp[src][lineInfo.v], dp[src][tmp] + lineInfo.q);
-                    if (lineInfo.v == dst){
+                    // 如果此时src->lineInfo.v 已经有比当前从tmp转移过来的既小且少的
+                    for (int j = 0; j < count + 1; j++) {
+                        if (dp[src][lineInfo.v][j] <= dp[src][tmp][count] + lineInfo.q) {
+                            continue Loop1;
+                        } else {
+                            dp[src][lineInfo.v][count + 1] = dp[src][tmp][count] + lineInfo.q;
+                        }
+                    }
+                    if (lineInfo.v == dst) {
                         continue;
                     }
                     list.add(lineInfo.v);
@@ -75,13 +90,13 @@ public class Solution_787 {
             count++;
             queue.addAll(list);
         }
-        if (dp[src][dst] == Integer.MAX_VALUE) {
-            return -1;
+        for (int i = 0; i <= K + 1; i++) {
+            minTotal = Math.min(dp[src][dst][i], minTotal);
         }
-        return dp[src][dst];
+        return minTotal == Integer.MAX_VALUE ? -1 : minTotal;
     }
 
-    int minTotal = Integer.MAX_VALUE;
+
     // 就是给定一个有向无环加权图，给定起始点，和终点，找到起始点到终点的所有路径
     // 然后再通过给定的K过滤掉那些不合法的数据
     // 再从合法的数据中找到最短的那条
