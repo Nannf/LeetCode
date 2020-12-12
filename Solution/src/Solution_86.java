05c641af32d1f695da76a5a989e6da0695ab39cd
Nannf <akmd_szc@163.com>
2020-12-12 23:27:01 +0800

    86

---
+++ Solution/src/Solution_86.java
@@ -0,0 +1,24 @@
+/**
+ * @auth Nannf
+ * @date 2020/12/12 23:26
+ * @description:
+ *
+ * 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+ *
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ *
+ * 输入: head = 1->4->3->2->5->2, x = 3
+ * 输出: 1->2->2->4->3->5
+ */
+public class Solution_86 {
+
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+    public ListNode partition(ListNode head, int x) {
+        return null;
+    }
+}
