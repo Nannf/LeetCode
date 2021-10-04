5406f4b084aeadbe8707af402d464b6f96438076
nannf <akmd_szc@163.com>
2021-10-04 10:09:05 +0800

    118

---
+++ Solution/src/Solution_118.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+/**
+ * @author Nannf
+ * @version v1.0
+ * @date 2021/10/4 10:08
+ * @Description
+ *
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ *
+ * 输入: numRows = 5
+ * 输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ *
+ * 输入: numRows = 1
+ * 输出: [[1]]
+ *
+ * 1 <= numRows <= 30
+ */
+public class Solution_118 {
+
+
+    public List<List<Integer>> generate(int numRows) {
+
+
+        return null;
+    }
+}
