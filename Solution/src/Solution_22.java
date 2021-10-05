a69ac30dbc12e95ffad3386e6546f75028f330c1
nannf <akmd_szc@163.com>
2021-10-05 20:36:56 +0800

    solution_22

---
+++ Solution/src/Solution_22.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+/**
+ * @author Nannf
+ * @version v1.0
+ * @date 2021/10/5 20:36
+ * @Description
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
+ * 有效括号组合需满足：左括号必须以正确的顺序闭合。
+ *
+ *输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ *
+ * 输入：n = 1
+ * 输出：["()"]
+ *
+ * 1 <= n <= 8
+ */
+public class Solution_22 {
+
+    public List<String> generateParenthesis(int n) {
+
+        return null;
+    }
+}
